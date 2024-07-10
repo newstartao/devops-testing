0bff14fb43a80893bdfda04044c5c347d293650e
mahipalsinghh <mahipal.singh@alyssum.global>
2024-07-10 18:47:29 +0530

    test

--- deploy/test.sh
+++ deploy/test.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "testing"
-echo "test.sh"
-
-
-sudo apt-get install $super_secret
-sudo systemctl start $super_secret
-
-sudo systemctl status $super_secret
+tool= $super_secret
+echo"$tool"
