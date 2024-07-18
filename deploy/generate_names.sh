514dd3a60f3492d87f5d37c6b901c74751c0f7de
jitips-devops <158477855+jitips-devops@users.noreply.github.com>
2024-07-18 15:47:59 +0530

    Update generate_names.sh

--- deploy/generate_names.sh
+++ deploy/generate_names.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Split the NAMES environment variable into an array
-IFS=' ' read -r -a names_array <<< "$NAMES"
+IFS=',' read -r -a names_array <<< "$NAMES"
 
 # Initialize the output string
 output="["
 
 # Loop through all the names in the array
 for name in "${names_array[@]}"; do
-  output+="\"$name\" "
+  output+="\"$name\", "
 done
 
 # Remove the trailing comma and space, and add the closing bracket
