3e47b5086d25403c88e3b4903eefc7ed5a5d4ff6
jitips-devops <158477855+jitips-devops@users.noreply.github.com>
2024-07-18 15:59:41 +0530

    Update generate_names.sh

--- deploy/generate_names.sh
+++ deploy/generate_names.sh
@@ -8,8 +8,9 @@ output="["
 
 # Loop through all the names in the array
 for name in "${names_array[@]}"; do
-  trimmed_name=$(echo "$name" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-  output+="$trimmed_name, "
+ # trimmed_name=$(echo "$name" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+ # output+="$trimmed_name, "
+  output+="\"$name\", "
 done
 
 # Remove the trailing comma and space, and add the closing bracket
